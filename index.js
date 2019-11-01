a5a318bdaf072b0cd5898004c659f127c78544ee
arcaso17 <muhanaariana27@gmail.com>
2019-11-01 19:52:49 -0300

    Subimos las funciones

---
+++ index.js
@@ -0,0 +1,18 @@
+var numero1 = parseInt(prompt("ingresa el primer numero de la suma"));
+var numero2 = parseInt(prompt("ingresa el segundo numero de la suma"));
+
+var resultado = sumarDosNumeros(numero1, numero2);
+alert(resultado);
+
+function sumarDosNumeros(x,y){
+    var i= 2;
+    return i+x+y;
+}
+
+var resultado = multiplicar(numero1, numero2);
+alert(resultado);
+
+function multiplicar(x,y){
+    return x*y;
+}
+
